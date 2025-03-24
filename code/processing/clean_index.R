da298c1cab70a9c9142980ade03067f5553cbe57
ewute <ewu@bates.edu>
2025-03-24 14:50:01 -0400

    Apply manga index

--- code/processing/clean_index.R
+++ code/processing/clean_index.R
@@ -1,30 +1,47 @@
 source('housekeeping.R')
 
 # Read in .csv files
-index <- read_csv(file.path(raw_dir, "anime_data.csv"))
+anime_index <- read_csv(file.path(raw_dir, "anime_data.csv"))
+manga_index <- read_csv(file.path(raw_dir, "manga_data.csv"))
 
 # Remove "N/A" values from the index
-index <- subset(index, !apply(index == "N/A", 1, any, na.rm = TRUE))
+anime_index <- subset(anime_index, !apply(anime_index == "N/A", 1, any, na.rm = TRUE))
+manga_index <- subset(manga_index, !apply(manga_index == "N/A", 1, any, na.rm = TRUE))
 
 # Make headings lowercase for easier merging
-index <- index %>%
+anime_index <- anime_index %>%
+  rename_all(tolower)
+
+manga_index <- manga_index %>%
   rename_all(tolower)
 
 # Remove headings with "anime_"
-index <- index %>%
+anime_index <- anime_index %>%
   rename_all(~gsub("anime_", "", .))
 
+# Remove headings with "manga_"
+manga_index <- manga_index %>%
+  rename_all(~gsub("manga_", "", .))
+
 # Change meanscore to mean_score
-index <- index %>%
+anime_index <- anime_index %>%
+  rename(mean_score = meanscore)
+manga_index <- manga_index %>%
   rename(mean_score = meanscore)
 
 # Change startdate and enddate to start_date and end_date
-index <- index %>%
+anime_index <- anime_index %>%
+  rename(start_date = startdate, end_date = enddate)
+
+manga_index <- manga_index %>%
   rename(start_date = startdate, end_date = enddate)
 
 # Drop columns id, mal_id
-index <- index %>%
+anime_index <- anime_index %>%
+  select(-id, -mal_id)
+manga_index <- manga_index %>%
   select(-id, -mal_id)
 
 # Save the cleaned index
-write_csv(index, file.path(clean_dir, "index_cleaned.csv"))
+write_csv(anime_index, file.path(clean_dir, "index_cleaned.csv"))
+write_csv(manga_index, file.path(clean_dir, "index_cleaned.csv"))
