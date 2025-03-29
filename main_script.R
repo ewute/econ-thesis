0cdddf6d783f912b708149e5985b2178f45f5e0b
ewute <ewu@bates.edu>
2025-03-29 15:32:42 -0400

    Main script outline

--- main_script.R
+++ main_script.R
@@ -1,31 +1,13 @@
-#File: main.Rmd or main.R
-#By: Kyle Coombs
-#What: Runs the project from start to finish in R from the Taylor Swift project. 
-#Date: 2023/09/12
-
-#Install packages with housekeeping. Also put together paths.
 source('housekeeping.R')
-#User written functions can be sourced -- or you could write a package, your call
-source(paste0(build,'clean_functions.R'))
-source(paste0(analysis,'analysis_functions.R'))
 
-#Import files
-source(paste0(build,'01_import_census.R'))
-source(paste0(build,'02_import_admin_data.R'))
+# Data processing
+# Clean charts and index
+source(file.path(code_dir, "processing", "clean_charts.R"))
+source(file.path(code_dir, "processing", "clean_index.R"))
 
-#Clean files
-source(paste0(build,'03_clean_census.R'))
-source(paste0(build,'04_clean_admin_data.R'))
+# Data analysis
+source(file.path(code_dir, "analysis", "fe_days_summary.R"))
 
-#Merge files 1 to 2
-source(paste0(build,'05_merge_census_admin.R'))
+# Data visualization
 
-#Analysis
-source('analysis/06_summary_stats.R')
-source('analysis/07_basic_regression.R')
-
-#Tables will likely be made with a host of R packages
-source('analysis/08_make_sum_figures.R')
-source('analysis/09_make_reg_figures.R')
-source('analysis/10_make_sum_tables.R')
-source('analysis/11_make_reg_tables.R')
+# Causal inference
